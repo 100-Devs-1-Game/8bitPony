93fce9d31522de003341fc9c21c78afbd8065103
Maniform <maniform.development@gmail.com>
2025-10-26 17:44:59 -0400

    Added hidden zone asset

---
+++ src/Scenes/Objects/HiddenZone/hidden_zone.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@export var layer: Node2D
+@export var end_alpha: float = 0.2
+@export var transition_time: float = 0.2
+
+var start_alpha: float
+var target_alpha: float
+
+
+func _ready() -> void:
+	start_alpha = layer.modulate.a
+	target_alpha = start_alpha
+
+
+func _process(_delta: float) -> void:
+	if layer.modulate.a != target_alpha:
+		layer.modulate.a = lerp(layer.modulate.a, target_alpha, transition_time)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		target_alpha = end_alpha
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		target_alpha = start_alpha
