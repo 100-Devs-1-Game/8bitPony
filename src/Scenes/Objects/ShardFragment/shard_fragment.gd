1b881e4c513666db0ae15a71eebb60a377044d57
kylamon1 <shawn_henry@pasd.us>
2025-12-20 12:18:15 -0500

    Update shard_fragment.gd
    takes shard_type, and id so we cand delete previously collected shards

--- src/Scenes/Objects/ShardFragment/shard_fragment.gd
+++ src/Scenes/Objects/ShardFragment/shard_fragment.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
 @onready var pick_up: AudioStreamPlayer = $PickUp
-@export var shard_type:Global.Shards
+var shard_type:Global.Gems
+var shard_id:int = 0
 
 func _on_body_entered(body):
 	if body is Player and visible:
 		visible = false
 		pick_up.play()
-		Global.pickup_shard(shard_type)
+		Global.pickup_shard(shard_type, shard_id)
 
 
 func _on_pick_up_finished():
