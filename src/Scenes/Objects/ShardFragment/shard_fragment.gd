9aa9d6ba03fa3662cb6d6c9c44df0fe9cbd461c6
kylamon1 <shawn_henry@pasd.us>
2025-12-20 06:50:35 -0500

    Shards now collect and can open doors

--- src/Scenes/Objects/ShardFragment/shard_fragment.gd
+++ src/Scenes/Objects/ShardFragment/shard_fragment.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
 @onready var pick_up: AudioStreamPlayer = $PickUp
-
+@export var shard_type:Global.Shards
 
 func _on_body_entered(body):
 	if body is Player and visible:
 		visible = false
 		pick_up.play()
-		Global.shard_counter += 1
+		Global.pickup_shard(shard_type)
 
 
 func _on_pick_up_finished():
--- src/Scripts/level_select.gd
+++ src/Scripts/level_select.gd
@@ -3,3 +3,9 @@ extends Node2D
 
 func _ready():
 	Global.room_tracker = Global.Room.LevelSelect
+	
+	for door in $Doors.get_children():
+		if Global.shard_collected[door.needed_shard]:
+			door.set_locked(false)
+		else:
+			door.set_locked(true)
