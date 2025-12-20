387982513cccc998eeed29317d9473f42563bfef
kylamon1 <shawn_henry@pasd.us>
2025-12-20 12:15:53 -0500

    Update main_menu.gd

--- src/Scripts/main_menu.gd
+++ src/Scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export_file ("*.tscn") var level_select_scene: String
+var testLevel = "res://Scenes/Rooms/feature_test_level.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,3 +9,7 @@ func _ready():
 
 func _on_start_button_pressed():
 	Global.root.load_scene(level_select_scene)
+
+
+func _on_test_level_pressed() -> void:
+	Global.root.load_scene(testLevel)
