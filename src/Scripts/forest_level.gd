9070ea51c1de97a62e1d12a0ec7b4161da7ab54b
kylamon1 <shawn_henry@pasd.us>
2025-12-20 06:36:50 -0500

    Renamed level script

--- src/Scripts/forest_level.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Global.room_tracker = Global.Room.Forest
-
-func _on_obj_killbox_body_entered(body):
-	if body.is_in_group("Player"):
-		body._take_damage() #should run the players damage function
