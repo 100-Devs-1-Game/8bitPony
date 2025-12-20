91579451f939203152e061b8c8737cc368d7b358
kylamon1 <shawn_henry@pasd.us>
2025-12-20 06:45:30 -0500

    Added export for required shard

--- src/Scenes/Objects/Door/door.gd
+++ src/Scenes/Objects/Door/door.gd
@@ -2,7 +2,8 @@ class_name Door
 extends Sprite2D
 
 @export var is_locked: bool = true
-@export var target_scene: PackedScene
+@export_file ("*.tscn") var target_scene:String
+@export var needed_shard:Global.Shards = Global.Shards.none
 
 @onready var lock_sprite: Sprite2D = $Lock
 @onready var tool_tip: Node2D = $ToolTip
@@ -12,16 +13,15 @@ var player_on_door: bool = false
 func _ready() -> void:
 	set_locked(is_locked)
 
-
 func set_locked(locked: bool):
 	is_locked = locked
 	lock_sprite.visible = locked
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
+	if body is Player and not is_locked:
 		player_on_door = true
-		tool_tip.visible = not is_locked
+		tool_tip.visible = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
@@ -31,5 +31,5 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Interact") and player_on_door and not is_locked:
+	if event.is_action_pressed("Interact") and player_on_door and not is_locked and Global.shard_collected[needed_shard]:
 		Global.root.load_scene(target_scene)
