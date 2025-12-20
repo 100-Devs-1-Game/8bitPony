b2c9b3bb6b90961cf8bc3f0ceb0da8b44ebe49d2
kylamon1 <shawn_henry@pasd.us>
2025-12-20 12:19:28 -0500

    Set player location based on gems collected

--- src/Scripts/level_select.gd
+++ src/Scripts/level_select.gd
@@ -5,7 +5,24 @@ func _ready():
 	Global.room_tracker = Global.Room.LevelSelect
 	
 	for door in $Doors.get_children():
-		if Global.shard_collected[door.needed_shard]:
+		if Global.gem_collected[door.needed_shard]:
 			door.set_locked(false)
 		else:
 			door.set_locked(true)
+		
+		if Global.gem_collected[Global.Gems.none]:
+			$player_start.global_position = $Doors/IntroDoor.global_position
+		if Global.gem_collected[Global.Gems.laughter]:
+			$player_start.global_position = $Doors/ForestDoor.global_position
+		if Global.gem_collected[Global.Gems.loyalty]:
+			$player_start.global_position = $Doors/LockedDoor1.global_position
+		if Global.gem_collected[Global.Gems.honesty]:
+			$player_start.global_position = $Doors/LockedDoor2.global_position
+		if Global.gem_collected[Global.Gems.generous]:
+			$player_start.global_position = $Doors/LockedDoor3.global_position
+		if Global.gem_collected[Global.Gems.kindness]:
+			$player_start.global_position = $Doors/LockedDoor4.global_position
+		if Global.gem_collected[Global.Gems.magic]:
+			$player_start.global_position = $Doors/LockedDoor5.global_position
+		if Global.gem_collected[Global.Gems.all]:
+			$player_start.global_position = $Doors/LockedDoor5.global_position
