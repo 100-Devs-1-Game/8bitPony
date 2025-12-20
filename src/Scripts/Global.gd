4bd588e29f27f367deff6b42cd339dfb7da51f2e
kylamon1 <shawn_henry@pasd.us>
2025-12-20 06:36:28 -0500

    Changed shard variables to an array

--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -15,15 +15,15 @@ var shard_counter: int = 0:
 var room_tracker: Room = Room.MainMenu
 
 # Element Gem trackers
-var laughter_gem: bool = false #if collected all the shards
-var loyalty_gem: bool = false #if collected all the shards
-var honesty_gem: bool = false #if collected all the shards
-var generous_gem: bool = false #if collected all the shards
-var kindness_gem: bool = false #if collected all the shards
-var magic_gem: bool = false #if collected all the shards
+enum Shards {laughter, loyalty, honesty, generous, kindness, magic, none}
+var shard_collected = [false, false, false, false, false, false, true]
 
 signal shard_value_changed(new_value: int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = false
+
+func pickup_shard(shard_type):
+	shard_collected[shard_type] = true
+	shard_counter+=1
