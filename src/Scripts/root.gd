f95b196aa6d1efc2ef9a7e68b63d00378f80475c
kylamon1 <shawn_henry@pasd.us>
2025-12-21 10:45:34 -0500

    Removed need for root...will be deleted soon

--- src/Scripts/root.gd
+++ src/Scripts/root.gd
@@ -3,12 +3,12 @@ extends Node
 
 @export_file ("*.tscn") var start_scene: String
 
-@onready var player: Player = $Player
-
 var current_scene: Node
 
-func _ready() -> void:
+func _enter_tree() -> void:
 	Global.root = self
+	
+func _ready() -> void:
 	load_scene(start_scene)
 
 
@@ -19,7 +19,7 @@ func load_scene(next_scene: String):
 	if next_scene:
 		var loaded_scene: Node = new_scene.instantiate()
 		if loaded_scene:
-			player.enabled = false
+			Global.player.enabled = false
 			if current_scene:
 				current_scene.queue_free()
 			current_scene = loaded_scene
@@ -28,18 +28,18 @@ func load_scene(next_scene: String):
 
 func _on_scene_changed():
 	var player_start: Node2D = current_scene.find_child("player_start")
-	if player_start:
-		player.global_position = player_start.global_position
-		player.visible = true
-		player.set_physics_process(true)
-		player.set_process(true)
-		player.set_process_unhandled_input(true)
-		player.camera.enabled = true
-		player.camera.reset_smoothing()
+	if Global.player:
+		Global.player.global_position = player_start.global_position
+		Global.player.visible = true
+		Global.player.set_physics_process(true)
+		Global.player.set_process(true)
+		Global.player.set_process_unhandled_input(true)
+		Global.player.camera.enabled = true
+		Global.player.camera.reset_smoothing()
 	else:
-		player.enabled = false
-		player.set_physics_process(false)
-		player.set_process(false)
-		player.set_process_unhandled_input(false)
-		player.visible = false
-		player.camera.enabled = false
+		Global.player.enabled = false
+		Global.player.set_physics_process(false)
+		Global.player.set_process(false)
+		Global.player.set_process_unhandled_input(false)
+		Global.player.visible = false
+		Global.player.camera.enabled = false
