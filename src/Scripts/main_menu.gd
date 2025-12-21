ca4718d593a1c1750669c5c1720de83aaecc5407
kylamon1 <shawn_henry@pasd.us>
2025-12-21 10:45:50 -0500

    Changed level loading

--- src/Scripts/main_menu.gd
+++ src/Scripts/main_menu.gd
@@ -3,13 +3,12 @@ extends Node
 @export_file ("*.tscn") var level_select_scene: String
 var testLevel = "res://Scenes/Rooms/feature_test_level.tscn"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Global.room_tracker = Global.Room.MainMenu
 
 func _on_start_button_pressed():
-	Global.root.load_scene(level_select_scene)
+	#Global.root.load_scene(level_select_scene)
+	get_tree().change_scene_to_file(level_select_scene)
 
 
 func _on_test_level_pressed() -> void:
-	Global.root.load_scene(testLevel)
+	#Global.root.load_scene(testLevel)
+	get_tree().change_scene_to_file(testLevel)
