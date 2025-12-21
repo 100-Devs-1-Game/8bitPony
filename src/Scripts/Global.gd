8b9c0809085e4ae6f89a60ac81595a767394a881
kylamon1 <shawn_henry@pasd.us>
2025-12-21 10:46:56 -0500

    Removed need for root scene.

--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -1,20 +1,34 @@
 extends Node
 
-enum Room {MainMenu, LevelSelect, Forest}
+enum Level {Forest, Apple, Cave, Library, Sugar, Cloud}
 
 var root: Root:
 	set(new_root):
 		root = new_root
-		player = root.player
+		
 var player: Player
+var current_form:int = 2 #Unicorn
+
 var scene: Node
+var score:int = 0:
+	set(value):
+		score = value
+		score_changed.emit()
+		
+var health:float = 3:
+	set(value):
+		health = value
+		health_changed.emit()
+
+
 var shard_counter: int = 0:
 	set(value):
 		shard_counter = value
 		shard_value_changed.emit(shard_counter)
-var room_tracker: Room = Room.MainMenu
+
 
 # Element Gem trackers
+		   #Green,   Red,     Blue,    Yellow,   Pink,     Purple
 enum Gems {laughter, loyalty, honesty, generous, kindness, magic, none, all}
 var gem_collected = [false, false, false, false, false, false, true, false]
 
@@ -28,6 +42,8 @@ var collected_shards = [[],
 			[],[]]
 
 signal shard_value_changed(new_value: int)
+signal score_changed(score:int)
+signal health_changed(health:float)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
