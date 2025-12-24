9e9910466a7324a6477a693670df825f36939c85
kylamon1 <shawn_henry@pasd.us>
2025-12-24 09:36:43 -0500

    Added toggle for if stalactites fall

--- src/Scenes/Objects/Cave/stalagtite_spawner.gd
+++ src/Scenes/Objects/Cave/stalagtite_spawner.gd
@@ -3,14 +3,16 @@ extends Area2D
 
 const STALAGTITE = preload("uid://mrt3k5iehyq2")
 var current_stag
+@export var allow_falling = true
 
 func _on_body_entered(_body: Node2D) -> void:
 	$drop_timer.start()
 
 
 func _on_drop_timer_timeout() -> void:
-	current_stag.fall()
-	$spawn_timer.start()
+	if allow_falling:
+		current_stag.fall()
+		$spawn_timer.start()
 	$CollisionShape2D.set_deferred("disabled", true)
 
 
