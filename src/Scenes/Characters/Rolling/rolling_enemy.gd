1738f7dbe78f1d453fc3ddf5c2c36c75ecc00eff
kylamon1 <shawn_henry@pasd.us>
2025-12-24 09:36:16 -0500

    Better pixel look on rolling enemy

--- src/Scenes/Characters/Rolling/rolling_enemy.gd
+++ src/Scenes/Characters/Rolling/rolling_enemy.gd
@@ -7,7 +7,8 @@ var sprite_type = "boulder"
 var gravity: float = 50
 var direction = -1
 var speed = 30
-
+var time_alive = 0
+var roll_angle = 0
 @onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
@@ -20,17 +21,28 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
-	
+	time_alive += delta
 	if is_on_floor():
 		velocity.x = direction * speed
-		sprite.rotation_degrees += 100*direction*delta
-		if sprite.rotation_degrees>360: sprite.rotation_degrees -=360
-		elif sprite.rotation_degrees<0: sprite.rotation_degrees +=360
+		roll_angle += 100*direction*delta
+		if roll_angle>360: roll_angle -=360
+		elif roll_angle<0: roll_angle +=360
+		
+		sprite.rotation_degrees = int(roll_angle/45)*90
+		
+		#if roll_angle <= 90: sprite.rotation_degrees = 0
+		#elif roll_angle <= 180: sprite.rotation_degrees = 90
+		#elif roll_angle <= 270: sprite.rotation_degrees = 180
+		#elif roll_angle <= 360: sprite.rotation_degrees = 270
 	else:
+		velocity.x = 0
 		velocity.y = gravity
 		
 	
 	move_and_slide()
+	
+	if time_alive > 30:
+		queue_free()
 
 
 
@@ -38,6 +50,5 @@ func _on_hurt_area_body_entered(body: Node2D) -> void:
 	body.take_damage()
 
 
-func _on_detect__wall_body_entered(body: Node2D) -> void:
-	print("Run into a wall...break")
+func _on_detect__wall_body_entered(_body: Node2D) -> void:
 	queue_free()
